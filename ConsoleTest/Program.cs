70c27992d172563ba8bc2d01aea867ae3071cc43
Joeytt-W <784725567@qq.com>
2022-03-08 17:47:14 +0800

    first commit

---
+++ ConsoleTest/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test test = new Test()
+            {
+                Name = "张三",
+                Age = 25
+            };
+
+            Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(test));
+            Console.WriteLine((int)TestEnum.SignMethods);
+        }
+    }
+
+
+    class Test
+    {
+        public string Name { get; set; }
+
+
+        public int Age { get; set; }
+    }
+
+    enum TestEnum
+    {
+        SignMethods = 1,
+        SignMethods2 = 2
+    }
+}
